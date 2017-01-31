5781856939dc6310fbbd20d96538a7be52939ebf
Ricky Brundritt <ricky_brundritt@hotmail.com>
2017-01-31 15:00:07 -0800

    Add default credentials

---
+++ Samples/BingMapsCredentials.js
@@ -0,0 +1,6 @@
+﻿//This file provides a Bing Maps key that is used by all the samples.
+
+//Update this value with your Bing Maps key.
+var YourBingMapsKey = 'YOUR_BING_MAPS_KEY';
+
+//Get your own Bing Maps key at https://www.bingmapsportal.com
