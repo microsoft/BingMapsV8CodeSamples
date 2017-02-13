4d9be5734012963ad9e632bdf3425e115eb8073d
Ricky Brundritt <ricky_brundritt@hotmail.com>
2017-02-13 11:58:30 -0800

    Updated list of external samples

--- Samples/ExternalSamples.js
+++ Samples/ExternalSamples.js
@@ -11,14 +11,11 @@ var externalSamples = {
     'Bots': [
         { title: 'Bing Location Control for Microsoft Bot Framework', 'href': 'https://github.com/Microsoft/BotBuilder-Location', description: 'An open-source location picker control for Microsoft Bot Framework powered by Bing Maps REST services. ' }
     ],
-    'JavaScript Frameworks': [
-        { title: 'Angular 2 implementation of Bing Maps V8', href: 'https://github.com/youjustgo/ng2-bingmaps' }
-    ],
     'Mobile': [
         { title: 'BingMapAndroid', href: 'https://github.com/LeonidVeremchuk/BingMapAndroid', description: 'An native Android SDK that wraps the Bing Maps V8 control.' }
     ],
     'Other': [
-        { title: 'DCProperties', href: 'https://github.com/rkgeorge/DCProperties', description: 'Example using Bing Maps v8 SpatialMath Module and DC GIS services' }
+        { title: 'DCProperties', href: 'https://github.com/rkgeorge/DCProperties', description: 'Example using Bing Maps v8 SpatialMath Module with the District of Columbia GIS services' }
     ],
     'Desktop': [
         { title: 'Using the Bing Maps V8 Web control in a WinForm application', href: 'https://code.msdn.microsoft.com/Using-the-Bing-Maps-V8-Web-07e21f3a' }
