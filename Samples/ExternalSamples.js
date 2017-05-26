521a0f753431de0f68ff315d4a52fd671ca20753
Ricky Brundritt <ricky_brundritt@hotmail.com>
2017-05-26 15:23:23 -0700

    update samples

--- Samples/ExternalSamples.js
+++ Samples/ExternalSamples.js
@@ -8,6 +8,9 @@
 */
 
 var externalSamples = {
+    'Angular':[
+        { title: 'Angular Maps (X-Map)', href: 'https://github.com/infusion-code/angular-maps', description: 'Angular Maps (X-Map) is a set of components and services to provide map functionality in angular 2+ apps. X-Maps architecture is provider independent and can be used with Bing, Google, ESRI or any other service enabled mapping provider. X-Map contains a default implementation for Bing Maps.' }
+    ],
     'Bots': [
         { title: 'Bing Location Control for Microsoft Bot Framework', 'href': 'https://github.com/Microsoft/BotBuilder-Location', description: 'An open-source location picker control for Microsoft Bot Framework powered by Bing Maps REST services. ' }
     ],
