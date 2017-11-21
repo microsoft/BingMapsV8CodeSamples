9ce0c92b4097a2a7735530fe132e5a008f49a127
Ricky Brundritt <ricky_brundritt@hotmail.com>
2017-11-20 17:27:10 -0800

    Update samples

--- Samples/Other/Angular16/app.js
+++ Samples/Other/Angular16/app.js
@@ -1,3 +1,22 @@
-﻿'use strict';
+﻿var testBingMap = angular.module('testBingMap', ['ngBMap']);
+testBingMap.controller('MainCtrl', function ($scope) {
 
-var ngBMap = angular.module('ngBMap', []);  //map directives
+    $scope.title = "Bing Maps V8 with Angular 1.6 Sample";
+    $scope.center = [43.000, 13.0000];
+
+    $scope.pushpins = [{
+        latitude: 43.000,
+        longitude: 13.0000,
+        text: 'hi',
+        color: 'red'
+    }, {
+        latitude: 43.0005,
+        longitude: 13.0005,
+        text: 'ok',
+        color: 'blue'
+    }];
+
+    $scope.onPushpinClick = function (pin) {
+        alert(pin.color + ' pin clicked');
+    };
+});
---
+++ Samples/Other/RequireJS/index.js
@@ -0,0 +1,19 @@
+﻿requirejs.config({
+    'paths': {
+        'jquery': '//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.0.min',
+        'async': '//cdnjs.cloudflare.com/ajax/libs/requirejs-plugins/1.0.3/async'
+    }
+});
+
+require(
+    [
+        'jquery',
+        'async!https://www.bing.com/mapspreview/sdk/mapcontrol'
+        //Alternatively: 'async!https://www.bing.com/mapspreview/sdk/mapcontrol?key=[YOUR_BING_MAPS_KEY]'
+    ],
+    function () {
+        var map = new Microsoft.Maps.Map('#myMap', {
+            credentials: bingMapsKey
+        });
+    }
+);
